3a8d76b75d17a74867143ca81665bf03fe857e85
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-03 14:28:42 +0100

    feat: add card content and base interactivity

--- components/Roadmap.vue
+++ components/Roadmap.vue
@@ -1,32 +1,50 @@
 <script lang="ts" setup>
-
+const cards = [{
+  name: 'Milestone 1',
+  status: 'In Progress',
+  title: 'Transition from centralized storage platform to trusted storage network',
+  description: 'Following this, a transition from a centralized storage model to a platform-neutral network of trusted nodes is underway. This shift lays the groundwork for a permissionless storage protocol, enhances integration with Filecoin L1 providers, and aligns with web3-native use cases.',
+}, {
+  name: 'Milestone 2',
+  status: 'In Progress',
+  title: 'Permissioned hot storage network with early node participation',
+  description: 'The storage node network is expanding to include third-party nodes on a permissioned basis, i.e. Filecoin L1 Storage Providers (SPs) or previous Saturn node providers. A verification system is being implemented to ensure that third-party providers maintain service guarantees. This system is crucial as it provides users with necessary proofs of service integrity.',
+}, {
+  name: 'Milestone 3',
+  status: 'In Progress',
+  title: 'Permissionless hot storage layer',
+  description: 'The network is transitioning to a permissionless model, allowing any node to participate in the network. This model is designed to be more inclusive and decentralized, providing a more robust and secure network. The transition to a permissionless model is a significant milestone in the development of the Storacha network.',
+}, {
+  name: 'Milestone 4',
+  status: 'In Progress',
+  title: 'Launching a retrieval network at scale',
+  description: 'In this milestone, the capabilities of the decentralized retrieval network are built upon and massively expanded. The retrieval network becomes open and permissionless to enable massive scaling, similar to the developments observed in the Saturn network. The storage network is further scaled.',
+}]
 </script>
 
 <template>
-  <div class="flex flex-col sm:flex-row">
+  <div class="flex flex-col gap-12 sm:flex-row">
     <div class="w-80">
       <Heading type="h3" class="color-brand-3">
-        Roadmap
+        Our Sizzling Roadmap
       </Heading>
-      <p class="p1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, porro? Fuga, repellat quae quos voluptatibus suscipit aut quod illum dolorem fugiat ex itaque adipisci alias obcaecati laudantium atque, aperiam repudiandae.
+      <p class="mt-4 text-base color-brand-3 p1">
+        Check out how we’re cranking up the heat with upcoming features and innovations to make your data storage experience scorching hot!
       </p>
     </div>
     <div class="cards">
-      <div class="card">
-        1
-      </div>
-      <div class="card">
-        2
-      </div>
-      <div class="card">
-        3
-      </div>
-      <div class="card">
-        4
-      </div>
-      <div class="card">
-        5
+      <div v-for="card in cards" :key="card.name" class="card">
+        <div class="pill">
+          <span class="b-1 rounded-full px-4 py-2 p3 text-xs color-brand-4">{{ card.status }}</span>
+        </div>
+        <div class="card-body mt-8 color-brand-4">
+          <Heading type="h4">
+            {{ card.title }}
+          </Heading>
+          <p class="mt-6 text-base p1">
+            {{ card.description }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -35,59 +53,46 @@
 <style lang="postcss" scoped>
 .cards {
   display: grid;
-  grid-auto-columns: 100%;
+  grid-auto-columns: calc(100% - 2rem);
   grid-column-gap: 10px;
   grid-auto-flow: column;
   padding: 25px 0px;
   list-style: none;
   overflow-x: scroll;
   scroll-snap-type: x mandatory;
+  width: 100%;
+
 }
 .card {
   display: flex;
   flex-direction: column;
   padding: 20px;
   border-radius: 12px;
-  scroll-snap-align: start;
+  scroll-snap-align: center;
   transition: all 0.2s;
-  background-color: red;
+  @apply bg-brand-3;
 }
 
-.cards::-webkit-scrollbar {
+@media (min-width: 1000px) {
+  .cards::-webkit-scrollbar {
   height: 12px;
-}
+  }
 
-.cards::-webkit-scrollbar-thumb,
-.cards::-webkit-scrollbar-track {
-  border-radius: 92px;
-}
+  .cards::-webkit-scrollbar-thumb,
+  .cards::-webkit-scrollbar-track {
+    border-radius: 92px;
+  }
 
-.cards::-webkit-scrollbar-thumb {
-  background: red;
-}
+  .cards::-webkit-scrollbar-thumb {
+    background: #eee;
+  }
 
-.cards::-webkit-scrollbar-track {
-  background: black;
-}
-
-@media (min-width: 500px) {
+  .cards::-webkit-scrollbar-track {
+    background: #ddd;
+  }
   .cards {
-    grid-auto-columns: calc(50% - 10px);
+    grid-auto-columns: calc(50% - 5rem);
     grid-column-gap: 20px;
-  }
-}
-
-@media (min-width: 700px) {
-  .cards {
-    grid-auto-columns: calc(calc(100% / 3) - 20px);
-    grid-column-gap: 30px;
-  }
-}
-
-@media (min-width: 1100px) {
-  .cards {
-    grid-auto-columns: calc(25% - 30px);
-    grid-column-gap: 40px;
   }
 }
 </style>

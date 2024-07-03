25e7cf11d70a3b50a99c1529b5331a4a5e4d77fb
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-03 19:31:08 +0100

    feat: roadmap block (#1)

---
+++ components/Roadmap.vue
@@ -0,0 +1,183 @@
+<script lang="ts" setup>
+const cards = [{
+  name: 'Milestone 1',
+  status: 'In Progress 🔥',
+  active: true,
+  title: 'From centralized storage to trusted storage network',
+  description: 'Following this, a transition from a centralized storage model to a platform-neutral network of trusted nodes is underway. This shift lays the groundwork for a permissionless storage protocol, enhances integration with Filecoin L1 providers, and aligns with web3-native use cases.',
+}, {
+  name: 'Milestone 2',
+  status: 'Up Next',
+  title: 'Permissioned hot storage network with early participation',
+  description: 'The storage node network is expanding to include third-party nodes on a permissioned basis, i.e. Filecoin L1 Storage Providers (SPs) or previous Saturn node providers. A verification system is being implemented to ensure that third-party providers maintain service guarantees. This system is crucial as it provides users with necessary proofs of service integrity.',
+}, {
+  name: 'Milestone 3',
+  status: 'Future',
+  title: 'Permissionless hot storage layer with retrieval MVP',
+  description: 'The network is transitioning to a permissionless model, allowing any node to participate in the network. This model is designed to be more inclusive and decentralized, providing a more robust and secure network. The transition to a permissionless model is a significant milestone in the development of the Storacha network.',
+}, {
+  name: 'Milestone 4',
+  status: 'Future',
+  title: 'Open and permissionless retrieval network at scale',
+  description: 'In this milestone, the capabilities of the decentralized retrieval network are built upon and massively expanded. The retrieval network becomes open and permissionless to enable massive scaling, similar to the developments observed in the Saturn network. The storage network is further scaled.',
+}]
+
+const isDragging = ref(false)
+const cursorPos = ref([0, 0])
+const el = ref<Element>()
+
+function onMouseDown(ev: MouseEvent) {
+  isDragging.value = true
+  cursorPos.value = [ev.clientX, ev.clientY]
+  window.addEventListener('mousemove', onMouseHold)
+}
+
+function onMouseUp() {
+  window.removeEventListener('mousemove', onMouseHold)
+  isDragging.value = false
+}
+
+function onMouseHold(ev: MouseEvent) {
+  ev.preventDefault()
+
+  requestAnimationFrame(() => {
+    const delta = [
+      ev.pageX - cursorPos.value[0],
+      ev.pageY - cursorPos.value[1],
+    ]
+
+    cursorPos.value = [ev.pageX, ev.pageY]
+
+    if (el.value) {
+      el.value.scrollBy({
+        left: -delta[0],
+        top: -delta[1],
+      })
+    }
+  })
+}
+
+onMounted(() => {
+  window.addEventListener('mouseup', onMouseUp)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('mouseup', onMouseUp)
+})
+</script>
+
+<template>
+  <div class="flex flex-col gap-12 2xl:grid-margins sm:flex-row">
+    <div class="w-80 px-4">
+      <Heading type="h3" class="color-brand-3 font-medium">
+        Our Sizzling Roadmap
+      </Heading>
+      <p class="mt-4 text-base color-brand-3 p1">
+        Check out how we’re cranking up the heat with upcoming features and innovations to make your data storage experience scorching hot!
+      </p>
+    </div>
+    <div
+      ref="el"
+      class="cards" :style="{
+        cursor: isDragging ? 'grabbing' : 'grab',
+        scrollSnapType: isDragging ? '' : '',
+      }"
+      :class="{ 'is-dragging': isDragging }"
+      @mousedown="onMouseDown"
+      @mouseup="onMouseUp"
+    >
+      <div v-for="card in cards" :key="card.name" class="card p-8">
+        <div class="pill" :class="{ 'pill-active': card.active }">
+          <span class="b-1 rounded-full px-4 py-2 p3 text-xs">{{ card.status }}</span>
+        </div>
+        <div class="card-body mt-8 color-brand-4">
+          <Heading type="h3">
+            {{ card.title }}
+          </Heading>
+          <p class="mt-6 text-sm p1 sm:text-base">
+            {{ card.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.pill span {
+  @apply color-brand-4 uppercase text-xs;
+}
+
+.pill-active span {
+  @apply color-brand-3 bg-brand-4;
+}
+
+.cards {
+  display: grid;
+  grid-auto-columns: calc(100% - 2rem);
+  grid-column-gap: 1rem;
+  grid-auto-flow: column;
+  list-style: none;
+  overflow-x: scroll;
+  width: 100%;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  scroll-snap-align: center;
+  transition: all 0.2s;
+  margin-bottom: 1rem;
+  @apply bg-brand-3 rounded-3rem py-15%;
+}
+
+.cards::-webkit-scrollbar {
+  height: 0.5rem;
+}
+
+.cards::-webkit-scrollbar-thumb,
+.cards::-webkit-scrollbar-track {
+  @apply rounded-3xl;
+}
+
+.cards::-webkit-scrollbar-thumb {
+  @apply bg-brand-3;
+}
+
+.cards::-webkit-scrollbar-track {
+  @apply bg-brand-4;
+}
+
+.cards.is-dragging {
+  user-select: none;
+}
+
+.card:last-child {
+  margin-right: 1rem;
+}
+
+@media (hover: none) {
+  .card {
+    margin: 0;
+  }
+  .cards {
+    scroll-snap-type: x mandatory;
+  }
+  .cards::-webkit-scrollbar {
+    display: none;
+  }
+  .card:first-child {
+    margin-left: 1rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  .card {
+    scroll-snap-align: start;
+  }
+  .cards {
+    grid-auto-columns: calc(45% - 5rem);
+    grid-column-gap: 1.5rem;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
             <h2 class="color-brand-3 font-500 h1">
               Storacha is decentralized storage, but a hot new take.
             </h2>
-            <div class="my-12 gap-12 color-brand-3 sm:columns-2 p1">
+            <div class="my-12 gap-12 color-brand-3 xl:columns-2 p1">
               <p>
                 Storacha transforms web3.storage into a community-driven, decentralized hot storage network. Proven to handle petabytes of data with large enterprise support, Storacha effortlessly manages massive data flows.
               </p>
@@ -36,6 +36,9 @@
           </div>
         </div>
       </div>
+    </Section>
+    <Section class="bg-brand-4 py-12" full-width>
+      <Roadmap />
     </Section>
     <Section full-width>
       <Community />

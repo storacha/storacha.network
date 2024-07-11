6442db3fae25fbb6ff92bc1174675820922d5072
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-11 11:59:47 +0100

    refactor: cleanup

--- server/api/feed.get.ts
+++ server/api/feed.get.ts
@@ -1,27 +1,26 @@
 import Parser from 'rss-parser'
 
+async function getFeed(username: string) {
+  const rss = await $fetch<string>(`https://medium.com/feed/@${username}`)
+  return rss
+}
+
+async function parseRSS(rss: string) {
+  const parser = new Parser()
+  const feed = await parser.parseString(rss)
+  return feed
+}
+
 async function fetchPosts(username: string) {
-  const getFeed = async () => {
-    const rss = await $fetch<string>(`https://medium.com/feed/@${username}`)
-    return rss
-  }
-
-  const parseRss = async (rss: string) => {
-    const parser = new Parser()
-    const feed = await parser.parseString(rss)
-    return feed
-  }
-
-  const rss = await getFeed()
-  const posts = await parseRss(rss) // parse rss to posts
-
+  const rss = await getFeed(username)
+  const posts = await parseRSS(rss)
   return posts
 }
 
 export default defineCachedEventHandler(async (event) => {
   const config = useRuntimeConfig(event)
   try {
-    const posts = await fetchPosts(config.public.medium as string)
+    const posts = await fetchPosts(config.public.medium)
     return posts
   }
   catch (err) {

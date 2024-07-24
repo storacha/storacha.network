c2e61c2b0df114da88f30963e56e1fd3da0120a3
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-24 20:02:43 +0100

    perf: use Boolean constructor in filter

--- server/api/blog.get.ts
+++ server/api/blog.get.ts
@@ -22,7 +22,7 @@ async function fetchPosts(username: string) {
     const images = Array.from(post['content:encoded']
       .matchAll(regex))
       .map(match => match[1])
-      .filter(url => !!url)
+      .filter(Boolean)
 
     return images.length ? { ...post, images } : post
   })

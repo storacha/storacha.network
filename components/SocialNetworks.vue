3c9432aa3d4483e05778eae08a038706e833834d
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-25 18:57:03 +0100

    add socialNetworks comp

---
+++ components/SocialNetworks.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const { socialNetworks } = useAppConfig()
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <div v-for="n in socialNetworks" :key="n.name">
+      <a class="h-10 w-10" :href="n.url" target="_blank" rel="noopener noreferrer">
+        <div :class="n.icon" :title="`${n.description} on ${n.name}`" />
+      </a>
+    </div>
+  </div>
+</template>

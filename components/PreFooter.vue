9954f3147a9b36e2e5198849fb540c97ce95a701
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-29 18:33:37 +0100

    sketch prefooter comp

---
+++ components/PreFooter.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+interface Props {
+  heading?: string
+  cta?: {
+    text: string
+    href: string
+  }
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="pre-footer min-h-">
+    <div class="grid-margins">
+      <div class="flex flex-col items-center justify-center gap-8 sm:flex-row">
+        <Heading v-if="heading" type="h4">
+          {{ heading }}
+        </Heading><AppLink v-if="cta" :href="cta.href">
+          {{ cta.text }}
+        </AppLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+
+</style>

a4482bf3ae0af59bfc1f5d08cfa2976ab4651b99
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-30 16:10:57 +0100

    sketch out pricing card and content

---
+++ components/Pricing.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <Section class="bg-brand-4" full-width padding>
+    <div class="grid-margins">
+      <SectionHeader
+        eyebrow="Find your plan"
+        title="How h🔥t would you like it?"
+        class="text-center color-brand-3"
+      />
+      <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
+        <PricingCard
+          title="Mild"
+          price="0"
+          unit="mo"
+          :features="[
+            '5GB Storage',
+            'Additional at $0.15/GB per month',
+            '5GB egress',
+            'Additional at $0.15/GB per month',
+          ]"
+          action="START STORING"
+        />
+        <PricingCard
+          title="Medium"
+          price="10"
+          unit="mo"
+          :features="[
+            '100GB Storage',
+            'Additional at $0.05/GB per month',
+            '100GB egress',
+            'Additional at $0.05/GB per month ',
+          ]"
+          action="START STORING"
+        />
+        <PricingCard
+          title="Extra Spicy"
+          price="100"
+          unit="mo"
+          :features="[
+            '2TB Storage',
+            'Additional at $0.03/GB per month',
+            '2TB egress',
+            'Additional at $0.03/GB per month',
+          ]"
+          action="START STORING"
+        />
+      </div>
+    </div>
+  </Section>
+</template>
+
+<style>
+
+</style>
---
+++ components/PricingCard.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+interface PricingCardProps {
+  title: string
+  description?: string
+  price: string
+  features: string[]
+  unit: 'mo' | 'yr'
+}
+
+defineProps<PricingCardProps>()
+</script>
+
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <p>{{ description }}</p>
+    <p>{{ price }}</p>
+    <ul>
+      <li v-for="feature in features" :key="feature">
+        {{ feature }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+
+</style>

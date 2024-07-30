47b39e736a67fb971bf0c59fa077a880681c2ac4
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-30 15:34:58 +0100

    setup section header comp

---
+++ components/SectionHeader.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import type { BtnProps } from './Btn.vue'
+
+interface Props {
+  title: string
+  eyebrow?: string
+  description?: string
+  actions?: BtnProps[] & { label: string }[]
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <Heading type="h5" class="uppercase">
+      {{ eyebrow }}
+    </Heading>
+    <Heading type="h2">
+      {{ title }}
+    </Heading>
+    <p class="prose">
+      {{ description }}
+    </p>
+    <div>
+      <Btn
+        v-for="action in actions"
+        :key="action.label"
+        v-bind="action"
+      >
+        {{ action.label }}
+      </Btn>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>

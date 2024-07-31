a48862f7c5c2f25e15a752217adbdce218dc5ba2
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-07-31 13:33:32 +0100

    style cleanup

--- components/Card.vue
+++ components/Card.vue
@@ -8,14 +8,14 @@ defineProps<CardProps>()
 </script>
 
 <template>
-  <div class="card" :class="[{ inherit: inheritColor }]">
+  <div class="card" :class="[{ 'is-inherit': inheritColor }]">
     <template v-if="title || icon">
       <slot name="header">
         <div class="card-header flex items-center justify-between">
           <Heading v-if="title" type="h6" class="uppercase">
             {{ title }}
           </Heading>
-          <i v-if="icon" class="icon">{{ icon }}</i>
+          <i v-if="icon" class="icon ml-a">{{ icon }}</i>
         </div>
       </slot>
       <div class="card-body">
@@ -30,12 +30,12 @@ defineProps<CardProps>()
   @apply b-1 color-brand-3 b-brand-3 bg-white rounded-lg flex-inline flex-col min-h-50;
 }
 .card-body {
-  @apply p-4 flex-grow color-current;
+  @apply p-4 grow color-current;
 }
 .card-header {
   @apply px-4 py-2 b-b-1 b-color-current;
 }
-.card.inherit {
+.card.is-inherit {
   @apply color-current b-color-current;
 }
 </style>

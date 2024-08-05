284da4feef8a9f5fb7b90653edc004ec80f56940
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-05 16:06:04 +0100

    feat: USPs comp (#13)
    * feat: usps comp
    
    * add usps to index
    
    * style: improve responsive grid

---
+++ components/USPs.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+const UPSs = [
+  {
+    icon: '🔥',
+    title: 'Kickstart your projects',
+    description: `Storacha takes the headache out of development. It's so easy to use that you'll be up and running in no time. Forget the boring stuff and dive straight into innovation!`,
+  },
+  {
+    icon: '🔥🔥',
+    title: 'Scale Like a Boss',
+    description: `With Storacha's spicy indexing and content discovery, scaling is a breeze. No more wrestling with infrastructure – just smooth, effortless growth.`,
+  },
+  {
+    icon: '🔥🔥🔥',
+    title: 'Blazing Fast Performance',
+    description: `Ultra-high storage throughput and super low retrieval times. Whether it's small files or massive data chunks, Storacha serves it hot and fast!`,
+  },
+  {
+    icon: '🔥🔥🔥🔥',
+    title: 'Truly Decentralized',
+    description: `With decentralized identity, you have true data ownership and can fine-tune permissions to decide who can do what with your data.`,
+  },
+]
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-12 lg:grid-cols-4 sm:grid-cols-2">
+    <div
+      v-for="{ icon, title, description } in UPSs"
+      :key="title"
+      class="grid row-span-3 grid-rows-subgrid gap-4"
+    >
+      <div class="h3">
+        {{ icon }}
+      </div>
+      <Heading type="h3" class="uppercase">
+        {{ title }}
+      </Heading>
+      <p class="p1">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style>
+
+</style>

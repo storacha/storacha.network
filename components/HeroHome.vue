13329fc3ecfd533223037d17476fd85b47562da8
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-05 17:56:40 +0100

    feat: home hero layout (#14)
    * export props from section header
    
    * refactor hero to use section header
    
    * hero -> herohome
    
    * class cleanup

---
+++ components/HeroHome.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useForwardProps } from 'radix-vue'
+import type { SectionHeaderProps } from './SectionHeader.vue'
+
+interface HeroProps extends SectionHeaderProps {}
+const props = withDefaults(defineProps<HeroProps>(), {})
+const forwardProps = useForwardProps(props)
+</script>
+
+<template>
+  <!-- hero -->
+  <div id="hero-bg" class="min-h-100vh flex">
+    <div class="w-full flex flex-col items-center justify-between sm:flex-row sm:gap-8">
+      <div class="mt-50 flex items-center sm:mt-0">
+        <div class="ml-a lg:max-w-220 sm:text-left">
+          <SectionHeader v-bind="forwardProps" class="color-brand-3 !gap-12" />
+        </div>
+      </div>
+      <div class="my-12 w-full" />
+    </div>
+  </div>
+</template>

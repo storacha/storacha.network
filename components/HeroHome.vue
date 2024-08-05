8ad849d9e12f30f00177d5dbed22ace735548f12
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-05 17:52:28 +0100

    class cleanup

--- components/HeroHome.vue
+++ components/HeroHome.vue
@@ -11,8 +11,8 @@ const forwardProps = useForwardProps(props)
   <!-- hero -->
   <div id="hero-bg" class="min-h-100vh flex">
     <div class="w-full flex flex-col items-center justify-between sm:flex-row sm:gap-8">
-      <div class="mt-50 flex items-center sm:mt-0 sm:min-h-100vh">
-        <div class="ml-a flex flex-1 flex-col gap-12 lg:max-w-220 sm:text-left">
+      <div class="mt-50 flex items-center sm:mt-0">
+        <div class="ml-a lg:max-w-220 sm:text-left">
           <SectionHeader v-bind="forwardProps" class="color-brand-3 !gap-12" />
         </div>
       </div>

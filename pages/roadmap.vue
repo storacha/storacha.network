582a7071ff18fb3d47ba5455f5a40347548e8c55
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-05 18:28:36 +0100

    feat: add roadmap route

---
+++ pages/roadmap.vue
@@ -0,0 +1,15 @@
+<template>
+  <TransitionProvider>
+    <Section class="bg-brand-4">
+      <Hero
+        class="min-h-150"
+        title="Roadmap"
+        description="Igniting the path to decentralized storage 🔥"
+        :actions="[{ label: 'Start Storing', href: '/' }, { label: 'Learn More', href: '/', secondary: true }]"
+      />
+    </Section>
+    <Section class="bg-brand-4" full-width padding>
+      <Roadmap />
+    </Section>
+  </TransitionProvider>
+</template>

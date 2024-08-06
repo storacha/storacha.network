09038dac06c3cd65ca7d6f5d1e18ad1f0fce8a93
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-06 16:19:06 +0100

    split comp cleanup

--- components/Split.vue
+++ components/Split.vue
@@ -1,9 +1,5 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
-  <div class="split grid auto-cols-max auto-rows-max cols-1 md:cols-2">
+  <div class="split grid cols-1 md:cols-2">
     <slot />
   </div>
 </template>

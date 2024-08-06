4e9a24c2d92ddf3532026ab4b5c5404789ee2208
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-06 14:37:26 +0100

    feat: implement split / cell comp

--- pages/roadmap.vue
+++ pages/roadmap.vue
@@ -8,6 +8,23 @@
         :actions="[{ label: 'Start Storing', href: '/' }, { label: 'Learn More', href: '/', secondary: true }]"
       />
     </Section>
+    <Section full-width>
+      <Split>
+        <SplitCell :bg="{ outer: 'bg-brand-2' }" />
+        <SplitCell :bg="{ outer: 'bg-white' }" padding>
+          <div class="flex items-center color-brand-3 md:my-24">
+            <div>
+              <Heading type="h2" class="font-medium">
+                About Storacha
+              </Heading>
+              <p class="mt-6 p1">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil commodi mollitia, obcaecati tempora ipsam velit cum fuga delectus iure optio molestias similique eius aliquam? Amet vel neque dolore nisi minus?
+              </p>
+            </div>
+          </div>
+        </SplitCell>
+      </Split>
+    </Section>
     <Section class="bg-brand-4" full-width padding>
       <Roadmap />
     </Section>

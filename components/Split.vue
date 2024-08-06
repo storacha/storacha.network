6a8ff2414774fd0c4cc718935fc58b0dcef82bde
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-06 14:37:10 +0100

    feat: add split / cell comp

---
+++ components/Split.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="split grid auto-cols-max auto-rows-max cols-1 md:cols-2">
+    <slot />
+  </div>
+</template>
---
+++ components/SplitCell.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+interface SplitCellProps {
+  bg?: {
+    outer?: string
+    inner?: string
+  }
+  padding?: boolean
+}
+defineProps<SplitCellProps>()
+</script>
+
+<template>
+  <div class="split-frame" :class="bg?.outer">
+    <div class="h-full md:max-w-160" :class="[bg?.inner, { 'p-12 px-6': padding }]">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="postcss">
+.split-frame > div {
+  margin-left: auto;
+}
+.split-frame + .split-frame > div {
+  margin-right: auto;
+  margin-left: 0;
+}
+</style>

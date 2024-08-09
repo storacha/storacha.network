1bc979c85be9456fb0cd79f3a32edec989c83d4f
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-09 20:36:34 +0200

    class cleanup

--- components/CommunityCell.vue
+++ components/CommunityCell.vue
@@ -10,13 +10,13 @@ const networks = appConfig.socialNetworks as SocialNetwork[]
 </script>
 
 <template>
-  <div class="w-full flex flex-col lg:ml-a">
+  <div class="w-full flex flex-col">
     <Heading type="h3" class="font-medium">
       Join the Community
     </Heading>
     <div class="my-a pt-8">
       <div class="networks grid grid-flow-dense gap-6 md:gap-12">
-        <AppLink v-for="n in networks" :key="n.name" class="flex items-center hover:color-brand-3" :href="n.url">
+        <AppLink v-for="n in networks" :key="n.name" class="flex items-center transition-all hover:(scale-102 color-brand-1)" :href="n.url">
           <div class="h-12 w-12 flex items-center justify-center border-1 b-current rounded-full">
             <div :class="n.icon" class="p-3" :aria-label="`${n.description} on ${n.name}`" />
           </div>

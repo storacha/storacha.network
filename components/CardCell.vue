8837a19547e6aa79339a4539f286d8bd29162b9f
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-09 19:45:34 +0100

    feat: community section (#17)
    * feat: add CardCell
    
    * rm community bg
    
    * use CardCell in Roadmap
    
    * add CommunityCell
    
    * feat: apply new Community section
    
    * rebuild pnpm-lock deps
    
    * style: responsive tweaks
    
    * class cleanup
    
    * sfc cleanup

---
+++ components/CardCell.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full rounded-20 px-10 py-20">
+    <slot />
+  </div>
+</template>
--- components/Community.vue
+++ components/Community.vue
@@ -1,67 +1,12 @@
-<script lang="ts" setup>
-interface SocialNetwork {
-  name: string
-  description: string
-  url: string
-  icon: string
-}
-// define props
-interface CommunityProps {
-  networks: SocialNetwork[]
-}
-const { networks } = defineProps<CommunityProps>()
-</script>
-
 <template>
-  <div class="grid grid-cols-1 box-border gap-0 bg-brand-1 lg:grid-cols-2">
-    <div class="s-block color-brand-1">
-      <div class="s-block-inner flex flex-col bg-brand-4 lg:ml-a">
-        <Heading type="h2" class="font-medium">
-          Join the Community
-        </Heading>
-        <div class="my-a py-8">
-          <div class="networks grid grid-flow-dense gap-6 md:gap-12">
-            <AppLink v-for="n in networks" :key="n.name" class="flex items-center hover:color-brand-3" :href="n.url">
-              <div class="h-12 w-12 flex items-center justify-center border-1 b-brand-1 rounded-full">
-                <div :class="n.icon" class="p-3" :aria-label="`${n.description} on ${n.name}`" />
-              </div>
-              <div class="px-4">
-                <Heading type="h4" class="font-500">
-                  {{ n.name }}
-                </Heading>
-                <p class="p3 text-sm">
-                  {{ n.description }}
-                </p>
-              </div>
-            </AppLink>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="s-block community-img">
+  <Split class="bg-brand-2">
+    <SplitCell padding>
+      <CardCell class="bg-brand-4">
+        <CommunityCell class="color-brand-3" />
+      </CardCell>
+    </SplitCell>
+    <SplitCell padding>
       <BlogCell />
-    </div>
-  </div>
+    </SplitCell>
+  </Split>
 </template>
-
-<style lang="postcss">
-.networks {
-  grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
-}
-.s-block {
-  @apply py-8 px-6 xl:p-12;
-}
-.s-block-inner {
-  @apply rounded-3xl p-8 md:p-12;
-  min-height: min(100vmin, 35rem);
-  width: 100%;
-}
-.s-block > div {
-  @apply lg:max-w-xl;
-}
-.community-img {
-  background-image: url('/img/community-bg.webp');
-  background-size: cover;
-  background-position: bottom;
-}
-</style>

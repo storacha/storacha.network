2b15aab247f556c59ef1d424d1eb959e25a53cde
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-10 13:07:01 +0200

    sketch out ecosystem header layout

--- components/Ecosystem.vue
+++ components/Ecosystem.vue
@@ -3,9 +3,15 @@
 </script>
 
 <template>
-  <div class="ecosystem">
-    <h1>Ecosystem</h1>
-  </div>
+  <SectionHeader
+    eyebrow="Get Involved"
+    title="Storacha is open sauce, it needs you. 🌶️"
+    description="Stay in touch and be one of the first to add some serious heat to your network with our blazing fast, decentralized storage and retrieval solutions!"
+    :actions="[{ label: 'BECOME A NODE PROVIDER', href: '/', primary: true }, { label: 'SIGN UP FOR OUR NEWSLETTER', href: '/', outline: true }]"
+  />
+  <Section class="ecosystem" padding>
+    <h1>THE STORACHA ECOSYSTEM</h1>
+  </Section>
 </template>
 
 <style>

3e7049b7c964a6c2f08f83f90da0fea3860e5b15
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-08-19 17:24:14 +0200

    rm unused classes

--- components/EcosystemCard.vue
+++ components/EcosystemCard.vue
@@ -16,14 +16,14 @@ defineProps<EcosystemCardProps>()
     <CategoryPill v-if="category" class="absolute right-4 top-4">
       {{ category.icon }} {{ category.name }}
     </CategoryPill>
-    <AvatarRoot class="bg-blackA3 h-[45px] w-[45px] inline-flex select-none items-center justify-center overflow-hidden rounded-full align-middle">
+    <AvatarRoot class="h-[45px] w-[45px] inline-flex select-none items-center justify-center overflow-hidden rounded-full align-middle">
       <AvatarImage
         class="h-full w-full rounded-[inherit] object-cover"
         src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
         alt="Colm Tuite"
       />
       <AvatarFallback
-        class="text-grass11 h-full w-full flex items-center justify-center bg-white text-[15px] font-medium leading-1"
+        class="h-full w-full flex items-center justify-center bg-white text-[15px] font-medium leading-1"
         :delay-ms="600"
       >
         CT

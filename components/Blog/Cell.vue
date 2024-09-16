8dc3ecc8991d63e27893bd3c64c25ddcaf5b0ae1
Chris Waring <106938+cwaring@users.noreply.github.com>
2024-09-16 18:28:15 +0100

    feat: setup homepage blog cell to feature two posts

--- components/Blog/Cell.vue
+++ components/Blog/Cell.vue
@@ -1,24 +1,37 @@
 <script lang="ts" setup>
 const { data: blog } = await useLazyFetch('/api/blog')
-// limit to 3 blog items
-const items = computed(() => blog.value?.items.slice(0, 3))
+// limit to 2 blog items
+const items = computed(() => blog.value?.items.slice(0, 2))
 </script>
 
 <template>
-  <div class="blog-cell grid gap-4 sm:cols-2">
-    <BlogCard
-      v-for="item in items"
-      :key="item.title"
-      :item="item"
-      class="grid-rows-subgrid"
-    />
+  <div class="h-full flex items-center justify-center">
+    <div>
+      <div class="blog-cell grid gap-4 sm:cols-2">
+        <BlogCard
+          v-for="item in items"
+          :key="item.title"
+          :item="item"
+          class="grid-rows-subgrid"
+        />
+      </div>
+      <div class="mt-10 flex items-center justify-center p1">
+        <AppLink href="/blog" primary class="flex items-center gap-1 lh-none">
+          View Storacha Blog<AppIcon i="i-carbon:arrow-right" />
+        </AppLink>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="postcss" scoped>
+/* featured item style */
+
+/*
 @screen sm {
   .blog-cell >:first-child {
     @apply col-span-2;
   }
 }
+*/
 </style>

01fc1dedd39598a754492bb102093b0fc88e4d31
Alan Shaw <alan.shaw@protocol.ai>
2024-09-20 17:06:47 +0100

    fix: use rss-to-json (#39)
    * fix: use rss-to-json
    
    * fix: pass in a URL
    
    * fix: use content_encoded field
    
    * fix: FML I hate this
    
    * fix: add back snippet

--- server/api/blog.get.ts
+++ server/api/blog.get.ts
@@ -1,33 +1,37 @@
-import Parser from 'rss-parser/dist/rss-parser.min.js'
-import type { Feed, Item } from '~/types/blog'
+import { XMLParser } from 'fast-xml-parser'
+import type { Feed } from '~/types/blog'
 
 async function getFeed(feedUrl: string) {
   const rss = await $fetch<string>(feedUrl)
   return rss
 }
 
-async function parseRSS(rss: string) {
-  const parser = new Parser<Feed, Item>()
-  const feed = await parser.parseString(rss)
-  return feed
-}
+async function fetchPosts(url: string): Promise<Feed> {
+  console.log('fetching posts')
+  const rss = await getFeed(url)
+  const root = new XMLParser().parse(rss)
+  const { channel } = root.rss
 
-async function fetchPosts(username: string) {
-  const rss = await getFeed(username)
-  const posts = await parseRSS(rss)
-
-  // transform posts and extract images to a new array key
-  // @ts-ignore
-  posts.items = posts.items.map((post) => {
-    const regex = /<img.*?src="(.*?)"/g
-    const images = Array.from(post['content:encoded']
-      // @ts-ignore
-      .matchAll(regex)).map(match => match[1]).filter(Boolean)
-
-    return images.length ? { ...post, images } : post
-  })
-
-  return posts
+  const regex = /<img.*?src="(.*?)"/g
+  return {
+    // transform posts and extract images to a new array key
+    items: channel.item.map((post: any) => {
+      const images = Array.from(String(post['content:encoded'])
+        .matchAll(regex)).map(match => match[1]).filter(Boolean)
+      let snippet = post['content:encoded'].replace(/(<([^>]+)>)/gi, '')
+      if (snippet.length > 200) {
+        snippet = snippet.slice(0, 200) + '...'
+      }
+      return {
+        title: post.title,
+        snippet,
+        pubDate: post.pubDate,
+        isoDate: post.isoDate,
+        link: post.link,
+        images
+      }
+    })
+  }
 }
 
 export default defineCachedEventHandler(async (event) => {
@@ -40,7 +44,7 @@ export default defineCachedEventHandler(async (event) => {
     return posts
   }
   catch (e: any) {
-    console.error(e)
+    console.error('failed to get blog posts:', e)
     // throw a generic error
     throw createError({ status: 500, message: `Failed to fetch posts: ${e.message}` })
   }
--- types/blog.ts
+++ types/blog.ts
@@ -1,28 +1,14 @@
 export interface Feed {
   items: Item[]
-  feedUrl: string
-  image: Image
-  paginationLinks: PaginationLinks
-  title: string
-  description: string
-  webMaster: string
-  generator: string
-  link: string
-  lastBuildDate: string
 }
 
 export interface Item {
-  'creator': string
-  'title': string
-  'link': string
-  'pubDate': string
-  'content:encoded': string
-  'content:encodedSnippet': string
-  'dc:creator': string
-  'guid': string
-  'categories': string[]
-  'isoDate': string
-  'images'?: string[]
+  title: string
+  snippet: string
+  link: string
+  pubDate: string
+  isoDate: string
+  images?: string[]
 }
 
 export interface Image {

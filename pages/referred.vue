2a58d679d18bbd470f8b60cfb4e4d5b2eb43ad40
Alan Shaw <alan@storacha.network>
2024-12-18 15:19:00 +0000

    fix: unneeded extra div

--- pages/referred.vue
+++ pages/referred.vue
@@ -10,18 +10,16 @@ const consoleSignupUrl = `${config.public.consoleUrl}?refcode=${refcode}`
     <Section class="bg-brand-4 referrals-bg">
       <HeroBase class="sm:min-h-200">
         <template #left>
-          <div>
-            <SectionHeader class="color-brand-3" eyebrow="A Master Racha Invited You"
-              title="Receive up to Two Months of Free Storage"
-              description="Sign up now for a Lite/Medium plan and receive two months subscription free. Sign up to a Business/Extra Spicy plan and receive one month subscription for free. Continue using Storacha to earn more rewards!" />
-            <div class="color-brand-3 prose">
-              <p>
-                For the full rundown on how it all works, check out our <b><a href="https://docs.storacha.network/referral-terms/" class="whitespace-nowrap">Terms &
-                    Conditions</a></b>.
-              </p>
-            </div>
-            <a :href="consoleSignupUrl" class="btn">Claim Your Discount</a>
+          <SectionHeader class="color-brand-3" eyebrow="A Master Racha Invited You"
+            title="Receive up to Two Months of Free Storage"
+            description="Sign up now for a Lite/Medium plan and receive two months subscription free. Sign up to a Business/Extra Spicy plan and receive one month subscription for free. Continue using Storacha to earn more rewards!" />
+          <div class="color-brand-3 prose">
+            <p>
+              For the full rundown on how it all works, check out our <b><a href="https://docs.storacha.network/referral-terms/" class="whitespace-nowrap">Terms &
+                  Conditions</a></b>.
+            </p>
           </div>
+          <a :href="consoleSignupUrl" class="btn">Claim Your Discount</a>
         </template>
       </HeroBase>
     </Section>

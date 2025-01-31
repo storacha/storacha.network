20406ebb280e26c70952254146c772a5b56321a0
ash <alan@storacha.network>
2025-01-31 17:08:14 +0000

    fix: remove transition on blog page (#121)
    For short screen heights the items simply don't appear!
    
    @cwaring do you have a better fix?

--- pages/blog.vue
+++ pages/blog.vue
@@ -4,28 +4,26 @@ const medium = useSocialNetwork('medium')
 </script>
 
 <template>
-  <TransitionProvider>
-    <Section class="bg-white" padding>
-      <div class="py-4 flex md:flex-row flex-col mt-20">
-        <div class="flex-none mb-4 md:mb-0">
-          <Heading type="h4" class="uppercase color-brand-3">
-            Blazing Hot News
-          </Heading>
-          <p class="max-w-50ch text-pretty prose p1 color-brand-3">The latest and greatest from the Storacha team.</p>
-        </div>
-        <div class="flex-auto md:text-right">
-          <Btn text="Follow on Medium" :href="medium?.href" />
-        </div>
+  <Section class="bg-white" padding>
+    <div class="py-4 flex md:flex-row flex-col mt-20">
+      <div class="flex-none mb-4 md:mb-0">
+        <Heading type="h4" class="uppercase color-brand-3">
+          Blazing Hot News
+        </Heading>
+        <p class="max-w-50ch text-pretty prose p1 color-brand-3">The latest and greatest from the Storacha team.</p>
       </div>
-      <div class="blog-cell grid gap-4 lg:cols-3 md:cols-2">
-        <BlogCard
-          v-for="item in blog?.items"
-          :key="item.title"
-          :item="item"
-          class="grid-rows-subgrid"
-          show-snippet
-        />
+      <div class="flex-auto md:text-right">
+        <Btn text="Follow on Medium" :href="medium?.href" />
       </div>
-    </Section>
-  </TransitionProvider>
+    </div>
+    <div class="blog-cell grid gap-4 lg:cols-3 md:cols-2">
+      <BlogCard
+        v-for="item in blog?.items"
+        :key="item.title"
+        :item="item"
+        class="grid-rows-subgrid"
+        show-snippet
+      />
+    </div>
+  </Section>
 </template>

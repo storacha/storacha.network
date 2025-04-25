126146868ef097630ae0b18c20bf5a3c7a03b8c0
Alan Shaw <alan@storacha.network>
2025-04-25 10:44:11 +0100

    feat: add hero images

--- pages/ecosystem.vue
+++ pages/ecosystem.vue
@@ -4,9 +4,9 @@
 
 <template>
   <TransitionProvider>
-    <Section class="bg-brand-4">
+    <Section class="ecosystem-bg bg-brand-4">
       <Hero
-        class="min-h-150"
+        class="min-h-150 lg:min-h-180"
         eyebrow="Learn More"
         title="Storacha Ecosystem"
         description="Discover how innovative projects are leveraging Storacha’s blazing-fast, secure, and scalable hot storage solution to push the boundaries of what's possible."
@@ -17,3 +17,22 @@
     </Section>
   </TransitionProvider>
 </template>
+
+<style lang="postcss" scoped>
+@screen md {
+  .ecosystem-bg {
+    background: url(/img/ecosystem/hero.png) no-repeat bottom right 10%;
+    background-size: auto 75%;
+  }
+}
+@screen lg {
+  .ecosystem-bg {
+    background-size: auto 78%;
+  }
+}
+@screen xl {
+  .ecosystem-bg {
+    background-size: auto 90%;
+  }
+}
+</style>
--- pages/referrals.vue
+++ pages/referrals.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import FreeStorage from '~/components/FreeStorage.vue';
-import ReferralCodeCreator from '~/components/ReferralCodeCreator.vue';
+import FreeStorage from '~/components/FreeStorage.vue'
+import ReferralCodeCreator from '~/components/ReferralCodeCreator.vue'
 </script>
 
 <template>
   <TransitionProvider>
-    <Section class="bg-brand-4 referrals-bg">
+    <Section class="referrals-bg bg-brand-4">
       <HeroBase class="sm:min-h-200">
         <template #left>
-          <SectionHeader class="color-brand-3" eyebrow="Earn Racha Points & Storage Credits"
+          <SectionHeader
+            class="color-brand-3" eyebrow="Earn Racha Points & Storage Credits"
             title="Refer your friends to Storacha!"
-            description="Share Storacha’s blazing-fast, decentralized storage and get rewarded with Racha Points, part of our soon to be announced Loyalty Program, and Storage Credits! It’s simple: the more friends you refer, the more perks you unlock!" />
+            description="Share Storacha’s blazing-fast, decentralized storage and get rewarded with Racha Points, part of our soon to be announced Loyalty Program, and Storage Credits! It’s simple: the more friends you refer, the more perks you unlock!"
+          />
           <div class="color-brand-3 prose p1">
             <p>
               <b>Enter your email</b> and we will generate your unique referral code!
@@ -22,7 +24,7 @@ import ReferralCodeCreator from '~/components/ReferralCodeCreator.vue';
     </Section>
     <Section full-width>
       <Split>
-        <SplitCell :bg="{ outer: 'bg-brand-2' }" />
+        <SplitCell :bg="{ outer: 'bg-brand-2', inner: 'rewards-bg' }" />
         <SplitCell :bg="{ outer: 'bg-white' }" padding>
           <div class="flex items-center color-brand-3">
             <div>
@@ -50,12 +52,16 @@ import ReferralCodeCreator from '~/components/ReferralCodeCreator.vue';
     <FreeStorage />
     <Section class="bg-brand-6 color-brand-3" padding>
       <ReferralProcess />
-      <ReferralCodeCreator class="pt-8"/>
+      <ReferralCodeCreator class="pt-8" />
     </Section>
   </TransitionProvider>
 </template>
 
-<style lang="postcss" scoped>
+<style lang="postcss">
+.rewards-bg {
+  background: url(/img/referrals/rewards.png) no-repeat bottom right;
+  background-size: contain;
+}
 @screen xl {
   .referrals-bg {
     background: url(/img/referrals/hero.png) no-repeat right;
